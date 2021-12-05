197a13ef8587b104582f1b75c78f29c21aa55ab1
LeDucLoi193 <dekaito193@gmail.com>
2021-12-05 17:06:03 +0700

    suggest product with age for dummy data

--- src/component/home.js
+++ src/component/home.js
@@ -1,119 +1,113 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React,{useState,useEffect} from 'react';
-import {Navbar, Table} from 'react-bootstrap';
-import {auth,db} from "../firebase";
-import {useNavigate} from "react-router-dom";
-import { MdArticle } from "react-icons/md";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React, { useState, useEffect } from 'react'
+import { Navbar, Table } from 'react-bootstrap'
+import { auth, db } from '../firebase'
+import { Link, useNavigate } from 'react-router-dom'
+import { MdArticle } from 'react-icons/md'
 
+export default function Home() {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [currentPage, setcurrentPage] = useState(1)
+  const [postPerPage] = useState(2)
+  const [users, setUsers] = useState([])
 
-export default function Home(){
- 
-    const[searchTerm,setSearchTerm] = useState('');
-    const[currentPage,setcurrentPage] = useState(1);
-    const[postPerPage] = useState(2);
-  const [users,setUsers] = useState([]);
+  const navigate = useNavigate()
+  useEffect(() => {
+    //Check user is logined
+    auth.onAuthStateChanged((user) => {
+      if (user != null) {
+      } else {
+        navigate('/login')
+      }
+    })
+  }, [])
 
-
-    const navigate = useNavigate();
-    useEffect(()=>{
-        //Check user is logined
-        auth.onAuthStateChanged((user)=>{
-            if(user!=null){
-
-            }
-            else{
-                navigate('/login');
-            }
+  useEffect(() => {
+    const getUserFromFB = []
+    db.collection('users')
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          getUserFromFB.push({ ...doc.data(), key: doc.id })
         })
-    },[])
-  
-    useEffect(() => {
-     const getUserFromFB = [];
-   db.collection("users").get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-          getUserFromFB.push({...doc.data(),key:doc.id,
-          });
-      });
-      setUsers(getUserFromFB);
-     
-  });
+        setUsers(getUserFromFB)
+      })
+  }, [])
 
+  //get currentPost
+  const indexofLast = currentPage * postPerPage
+  const indexofFirst = indexofLast - postPerPage
+  const currentPosts = users.slice(indexofFirst, indexofLast)
 
- },[]);
-    
-    //get currentPost
-    const indexofLast = currentPage*postPerPage;
-    const indexofFirst = indexofLast - postPerPage;
-    const currentPosts = users.slice(indexofFirst,indexofLast)
+  const pageNumber = []
+  for (let i = 1; i <= Math.ceil(users.length / postPerPage); i++) {
+    pageNumber.push(i)
+  }
+  const paginate = (pageNumber) => setcurrentPage(pageNumber)
+  return (
+    <div className="home">
+      <Navbar />
+      <input
+        type="text"
+        placeholder="検索 ...."
+        onChange={(event) => {
+          setSearchTerm(event.target.value)
+        }}
+      />
 
-   const pageNumber =[];
-   for(let i =1;i <= Math.ceil(users.length / postPerPage);i++){
-     pageNumber.push(i);
-   };
-   const paginate = (pageNumber) => setcurrentPage(pageNumber);
-    return(
-      
-        <div className="home">
-          <Navbar/>
-      <input type="text" placeholder="検索 ...."  onChange ={event => {setSearchTerm(event.target.value)} }/>
-
-          <Table striped bordered hover>
-          <thead>
-              <tr>
-                <td>番号</td>
-                <td>名前</td>
-                <td>年齢</td>
-                <td>性別</td>
-                <td>場所</td>
-                <td>メール</td>
-                <td>電話番号</td>
-                <td>アクション</td>
-              </tr>
-              </thead>
-          <tbody>
-          {
-            currentPosts.filter((val) =>{
-              if(searchTerm==""){
-                return val;
-              }else if(val.name.toLowerCase().includes(searchTerm.toLowerCase())){
-                return val;
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <td>番号</td>
+            <td>名前</td>
+            <td>年齢</td>
+            <td>性別</td>
+            <td>場所</td>
+            <td>メール</td>
+            <td>電話番号</td>
+            <td>アクション</td>
+          </tr>
+        </thead>
+        <tbody>
+          {currentPosts
+            .filter((val) => {
+              if (searchTerm == '') {
+                return val
+              } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                return val
               }
-            }).map((item,key) =>(
-                <tr key={key}>
+            })
+            .map((item, key) => (
+              <tr key={key}>
                 <td>{item.id}</td>
-                <td>{item.name} {item.ten}</td>
+                <td>
+                  {item.name} {item.ten}
+                </td>
                 <td>{item.age}</td>
                 <td>{item.gioitinh}</td>
                 <td>{item.addr}</td>
                 <td>{item.mail}</td>
                 <td>{item.phoneNumber}</td>
                 <td>
-            
-                  <a href ="/suggestion"> 
-                  <button >
-                  <MdArticle/>
-
+                  <button onClick={() => navigate('/suggestion', { state: { minAgeSuggest: item.age } })}>
+                    <MdArticle />
                   </button>
-                   </a>
                 </td>
               </tr>
-              ))}
-          </tbody>
-            </Table>
-            <nav>
-            <ul className='pagination justify-content-center'>
-                {
-                 pageNumber.map(number =>(
-                    <li key={number} className='page-item'>
-                      <a onClick={() => paginate(number)}  className='page-link' >
-                        {number}
-                      </a>
-
-                    </li>
-                  ))
-                }
-            </ul>
-            </nav>
-        </div>
-    );
+            ))}
+        </tbody>
+      </Table>
+      <nav>
+        <ul className="pagination justify-content-center">
+          {pageNumber.map((number) => (
+            <li key={number} className="page-item">
+              <a onClick={() => paginate(number)} className="page-link">
+                {number}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  )
 }
--- src/component/suggestion.js
+++ src/component/suggestion.js
@@ -1,35 +1,47 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { useNavigate } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import { dummyData } from '../data/suggestProduct'
-import {useEffect} from "react";
-import {auth} from "../firebase";
+import { useEffect } from 'react'
+import { auth } from '../firebase'
+import { Col, Container, Row } from 'react-bootstrap'
 
 export default function SuggestProduct() {
-    const navigate = useNavigate();
-    useEffect(()=>{
-        //Check user is logined
-        auth.onAuthStateChanged((user)=>{
-            if(user!==null){
-              
-            }
-            else{
-                navigate('/login');
-            }
-        })
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  let data = []
+  if (location.state?.minAgeSuggest) {
+    data = dummyData.filter((data) => data.minAgeSuggest >= location.state.minAgeSuggest)
+  } else data = dummyData
+
+  useEffect(() => {
+    //Check user is logined
+    auth.onAuthStateChanged((user) => {
+      if (user !== null) {
+      } else {
+        navigate('/login')
+      }
     })
+  })
+
   return (
     <div className="d-flex flex-column shadow-lg p-3 m-auto mt-5" style={{ maxWidth: '1200px' }}>
       <h1 className="text-center">Suggest Product</h1>
-      {dummyData.map((product) => (
-        <div className="d-flex align-items-center mt-4">
-          <img src={product.image} alt="product" />
-          <div className="d-flex flex-column" style={{ marginLeft: '16px' }}>
-            <h2>{product.name}</h2>
-            <h4>{product.price} $</h4>
-            <p>Category: {product.type}</p>
-          </div>
-        </div>
-      ))}
+      <Container>
+        <Row className="d-flex align-items-center mt-4">
+          {data.map((product) => (
+            <Col key={product.id} md={12} lg={6} className="d-flex align-items-center mb-4">
+              <img src={product.image} alt="product" />
+              <div className="d-flex flex-column" style={{ marginLeft: '16px' }}>
+                <h2>{product.name}</h2>
+                <h4>{product.price} $</h4>
+                <p>Category: {product.type}</p>
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   )
 }
