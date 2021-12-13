a18a7eb7289b5c8085b6b542b054aed7e01e9f67
LeDucLoi193 <dekaito193@gmail.com>
2021-12-13 22:00:45 +0700

    fetch product from firebase

--- src/component/suggestion.js
+++ src/component/suggestion.js
@@ -1,23 +1,37 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { useNavigate } from 'react-router-dom'
-import { useLocation } from 'react-router-dom'
-import { dummyData } from '../data/suggestProduct'
-import React, {useEffect, useState} from 'react'
-import { auth } from '../firebase'
+import { useNavigate, useLocation } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { auth, db } from '../firebase'
 import { Col, Container, Row } from 'react-bootstrap'
+
+const postPerPage = 6
 
 export default function SuggestProduct() {
   const navigate = useNavigate()
   const location = useLocation()
   const [currentPage, setCurrentPage] = useState(1)
-  const [postPerPage] = useState(10)
   const [searchTerm, setSearchTerm] = useState('')
   const [pageNumber, setPageNumber] = useState([])
   const [dataProduct, setDataProduct] = useState([])
-  let data = []
-  if (location.state?.minAgeSuggest) {
-    data = dummyData.filter((data) => data.minAgeSuggest >= location.state.minAgeSuggest)
-  } else data = dummyData
+  const [renderData, setRenderData] = useState([])
+
+  useEffect(() => {
+    const productsData = []
+    db.collection('products')
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          productsData.push({ ...doc.data(), key: doc.id })
+        })
+        setDataProduct(productsData)
+      })
+  }, [])
+
+  useEffect(() => {
+    if (location.state?.minAgeSuggest) {
+      setRenderData(dataProduct.filter((data) => data.minAgeSuggest >= location.state.minAgeSuggest))
+    } else setRenderData(dataProduct)
+  }, [dataProduct])
 
   useEffect(() => {
     //Check user is logined
@@ -28,48 +42,51 @@ export default function SuggestProduct() {
       }
     })
   })
-  useEffect(()=>{
+
+  useEffect(() => {
     const pageNumber1 = []
-    for (let i = 1; i <= Math.ceil(dataProduct.length / postPerPage); i++) {
+    for (let i = 1; i <= Math.ceil(renderData.length / postPerPage); i++) {
       pageNumber1.push(i)
     }
-    setPageNumber(pageNumber1);
-  },[dataProduct])
-  useEffect(()=>{
-    let dataRcv = data
-        .filter((val) => {
-          if (searchTerm == '') {
-            return val
-          } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
-            return val
-          }
-        });
-    setDataProduct(dataRcv);
-  },[searchTerm])
+    setPageNumber(pageNumber1)
+  }, [renderData])
+
+  useEffect(() => {
+    let dataRcv = dataProduct.filter((val) => {
+      if (searchTerm == '') {
+        return val
+      } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+        return val
+      }
+    })
+    setRenderData(dataRcv)
+  }, [searchTerm])
+
   //get currentPost
   const indexofLast = currentPage * postPerPage
   const indexofFirst = indexofLast - postPerPage
-  const currentPosts = dataProduct.slice(indexofFirst, indexofLast)
+  const currentPosts = renderData.slice(indexofFirst, indexofLast)
   const paginate = (pageNumber) => setCurrentPage(pageNumber)
+
   return (
     <div className="d-flex flex-column shadow-lg p-3 m-auto mt-5" style={{ maxWidth: '1200px' }}>
       <h1 className="text-center">おすすめ商品</h1>
       <input
-          type="text"
-          placeholder="検索 ...."
-          onChange={(event) => {
-            setSearchTerm(event.target.value)
-          }}
+        type="text"
+        placeholder="検索 ...."
+        onChange={(event) => {
+          setSearchTerm(event.target.value)
+        }}
       />
       <Container>
         <Row className="d-flex align-items-center mt-4">
           {currentPosts.map((product) => (
-            <Col key={product.id} md={12} lg={6} className="d-flex align-items-center mb-4">
+            <Col key={product.key} md={12} lg={6} className="d-flex align-items-center mb-4">
               <img src={product.image} alt="product" />
               <div className="d-flex flex-column" style={{ marginLeft: '16px' }}>
-                <h2>{product.name}</h2>
-                <h4>{product.price} $</h4>
-                <p>Category: {product.type}</p>
+                <h2>{product.productName}</h2>
+                <h4>{product.productPrice} $</h4>
+                <p>Category: {product.productNumber}</p>
               </div>
             </Col>
           ))}
@@ -77,11 +94,11 @@ export default function SuggestProduct() {
         <nav>
           <ul className="pagination justify-content-center">
             {pageNumber.map((number) => (
-                <li key={number} className="page-item">
-                  <a onClick={() => paginate(number)} className="page-link">
-                    {number}
-                  </a>
-                </li>
+              <li key={number} className="page-item">
+                <button onClick={() => paginate(number)} className="page-link">
+                  {number}
+                </button>
+              </li>
             ))}
           </ul>
         </nav>
